f9f26d784558459dbb96dc25863032fa501c637e
rafurity <raflialfajari@gmail.com>
2024-05-22 13:06:36 +0700

    - create fetch character data, constants file,and add ViewModel file (@MainActor, Observable Object, @Published, private(set)

---
+++ BB Quotes17Rafuri/Model/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  BB Quotes17Rafuri
+//
+//  Created by Macbook on 22/05/24.
+//
+
+import Foundation
---
+++ BB Quotes17Rafuri/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  BB Quotes17Rafuri
+//
+//  Created by Macbook on 22/05/24.
+//
+
+import Foundation
